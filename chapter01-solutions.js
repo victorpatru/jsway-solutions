95a5b4bf6cc88f22f86de8f2f2dfad02f774e1ce
Victor Patru <patruvictor.mihai@gmail.com>
2022-05-11 01:23:08 +0300

    Add chapter 1 and updated file naming

---
+++ chapter01-solutions.js
@@ -0,0 +1,19 @@
+// Presentation
+// Write a program that displays your name and age. Here's the result for mine.
+
+console.log('Victor')
+console.log(23)
+
+// Minimalistic calculator
+// Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3.
+console.log(6 + 3)
+console.log(6 - 3)
+console.log(6 / 3)
+
+// Values prediction
+// Observe the following program and try to predict the values it displays.
+
+console.log(4 + 5); // 9
+console.log("4 + 5"); // 4 + 5
+console.log("4" + "5"); // 45
+// Check your prediction by executing it.
